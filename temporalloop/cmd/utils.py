4e56d0da58cd2d5be22dd5a36de1c161d54cd85b
ant31 <2t.antoine@gmail.com>
2025-10-21 15:25:44 +0200

    fix: Propagate CLI host/namespace overrides to workers

--- temporalloop/cmd/utils.py
+++ temporalloop/cmd/utils.py
@@ -20,8 +20,6 @@ def load_config_with_overrides(config_path: Path, host: str | None, namespace: s
     if namespace is not None:
         config.temporalio.namespace = namespace
 
-    # Re-run the model validator to propagate the new host and namespace to workers
-    config.temporalio = config.temporalio.model_copy(
-        update={"host": config.temporalio.host, "namespace": config.temporalio.namespace}
-    )
+    # Manually re-apply the inheritance to propagate the new host and namespace to workers
+    config.temporalio.inherit_worker_settings()
     return config
